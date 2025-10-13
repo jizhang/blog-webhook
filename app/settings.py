d584cb916f9b34ab3d0ebdecd2fe242524f134ba
Jerry <zhangji87@gmail.com>
2025-10-13 18:40:35 +0800

    Deploy signed & secured

--- app/settings.py
+++ app/settings.py
@@ -8,7 +8,6 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env")
 
-    AUTH_TOKEN: str
     HMAC_SECRET: str
     DEPLOY_BASE: str
 
